fcfd03ca1a1953d226bb7e0c1f35054e35d1b344
Hesam Ro <hesamroustaei80@gmail.com>
2025-08-09 16:56:10 +0330

    Add all pages Frontend n Backend

--- main.py
+++ main.py
@@ -11,11 +11,30 @@ posters = c.fetchall()
 app = Flask(__name__)
 
 @app.route("/")
-def hello_flask():
+def home():
 
     return render_template("index.html", posters=posters)
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/poster/<poster_name>")
+def poster_page(poster_name):
+    with sqlite3.connect("item.db") as con:
+        c = con.cursor()
+        c.execute("SELECT * FROM items WHERE name = ?", (poster_name,))
+        current_poster = c.fetchall()[0]
+    
+    return render_template("poster_page.html", poster=current_poster)
+
+@app.route("/payment")
+def payment():
+    return render_template("payment.html")
 
 conn.close()
 if __name__ == "__main__":
