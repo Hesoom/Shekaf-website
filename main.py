567da6441be0d1d07984ac3a91035ea697030a50
Hesam Ro <hesamroustaei80@gmail.com>
2025-08-10 21:24:00 +0330

    Add Login system

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+import os
 
-conn = sqlite3.connect("item.db")
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, "item.db")
+conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
 c.execute("SELECT * FROM items")
@@ -9,19 +15,32 @@ posters = c.fetchall()
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = '192SD87HAKD89CQHDJQXCDAQ'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///items.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-
     return render_template("index.html", posters=posters)
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "2@2.com" and form.password.data == 'password':
+            flash(f"Successfuly Loged in",'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f"Wrong Email or Password")
+    return render_template("login.html", form=form)
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET","POST"])
 def signup():
-    return render_template("signup.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created",'success')
+        return redirect(url_for('home'))
+    return render_template("signup.html", form=form)
 
 @app.route("/poster/<poster_name>")
 def poster_page(poster_name):
