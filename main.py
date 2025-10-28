8d0e7bdc836e45972d3126e5a9e96671b09cb178
Hesam Ro <hesamroustaei80@gmail.com>
2025-10-28 22:27:35 +0330

    Add login signup system

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, redirect, url_for, flash
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from flask_login import UserMixin, login_user, login_required, current_user, logout_user
+from flask_login import UserMixin, login_user, login_required, current_user, logout_user, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '192SD87HAKD89CQHDJQXCDAQ'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///store.db'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +31,10 @@ class User(UserMixin, db.Model):
 with app.app_context():
     db.create_all()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route("/")
 def home():
     posters = Items.query.all()
@@ -35,14 +44,21 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        
-        
 
-        if form.email.data == "2@2.com" and form.password.data == 'password':
-            flash(f"Successfuly Loged in",'success')
-            return redirect(url_for('home'))
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                login_user(user)
+                return redirect(url_for("home"))
+            else:
+                flash("Invalid email or password")
+                return redirect(url_for('login'))
         else:
-            flash(f"Wrong Email or Password")
+            flash("Email doesn't exit")
+            return redirect(url_for('login'))
+
     return render_template("login.html", form=form)
 
 @app.route("/signup", methods=["GET","POST"])
@@ -54,7 +70,6 @@ def signup():
         existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             flash("Email already registered, please log in instead.")
-            print(existing_user.email)
             return redirect(url_for('login'))
 
         hashed_password = generate_password_hash(form.password.data,method='pbkdf2',salt_length=8)
@@ -72,6 +87,15 @@ def signup():
         return redirect(url_for('home'))
     
     return render_template("signup.html", form=form)
+
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user() 
+    flash("You have been logged out.", "success")
+    return redirect(url_for("home"))
 
 @app.route("/poster/<poster_name>")
 def poster_page(poster_name):
